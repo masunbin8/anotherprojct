04141ae61cbb895414530481b18b69886fe942b7
masunbin8 <masunbin@naver.com>
2023-02-21 16:34:35 +0900

    Dialog 기능 추가

--- public/js/logIn.js
+++ public/js/logIn.js
@@ -3,16 +3,32 @@
 const id = document.getElementById("id").value;
 const password = document.getElementById("password").value;
 
-function getLoginData() {    
-    if (id == "") {        
-        return alert("아이디를 입력해 주세요.");
-    }
-    if (password == "") {
-        return alert("비밀번호를 입력해 주세요.");        
-    }
-    console.log("ID: " + id);
-    console.log("Password: " + password);
+//Dialog
+const body = document.body;
+const openDialogBtn = document.getElementById("open-dialog");
+const closeDialogBtn = document.getElementById("close-dialog");
+const dialog = document.getElementById("my-dialog");
+
+
+function logIn() {    
+    console.log(id);
+    console.log(password);
+    
 }
 function signUp() {
     window.location.replace("signup.html");
 }
+
+function closeDialog() {
+    dialog.close();
+    body.style.opacity = "1";
+}
+
+
+/*
+    if (id == "" && password == "") {        
+        openDialogBtn.addEventListener("click", () => {
+            dialog.showModal();
+            body.style.opacity = "0.2";
+        });
+    } */   
