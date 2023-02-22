7335091d1a350db2af241b8c4b049e00fde6407b
masunbin8 <masunbin@naver.com>
2023-02-22 21:44:00 +0900

    .

--- public/js/logIn.js
+++ public/js/logIn.js
@@ -1,18 +1,36 @@
 'use strick';
 
-const id = document.getElementById("id").value;
-const password = document.getElementById("password").value;
+//Dialog
+const body = document.body;
+const openDialogBtn = document.getElementById("open-dialog");
+const closeDialogBtn = document.getElementById("close-dialog");
+const dialog = document.getElementById("my-dialog");
+console.log("login.js load.");
 
-function getLoginData() {    
-    if (id == "") {        
-        return alert("아이디를 입력해 주세요.");
-    }
-    if (password == "") {
-        return alert("비밀번호를 입력해 주세요.");        
-    }
-    console.log("ID: " + id);
-    console.log("Password: " + password);
+function logIn() {
+    const id = document.getElementById("id").value;
+    const password = document.getElementById("password").value;
+    if(id=="" ) {
+        dialog.showModal();
+        body.style.opacity = "0.2";
+    } 
+    console.log(password);
+    
 }
 function signUp() {
     window.location.replace("signup.html");
 }
+
+function closeDialog() {
+    dialog.close();
+    body.style.opacity = "1";
+}
+
+
+/*
+    if (id == "" && password == "") {        
+        openDialogBtn.addEventListener("click", () => {
+            dialog.showModal();
+            body.style.opacity = "0.2";
+        });
+    } */   
