8da8d136bd48fdbddd017a5ce84cb2dbca6712f5
masunbin8 <masunbin@naver.com>
2023-02-28 10:20:58 +0900

    modal component #1

--- public/js/logIn.js
+++ public/js/logIn.js
@@ -1,36 +1,72 @@
 'use strick';
 
-//Dialog
+function signUp() {
+    window.location.replace("signup.html");
+}
+/* Modal */
+const modal = document.getElementById("logInModal");
+const closeModalButton = document.getElementById("close-modal-button");
+
+function logIn() {
+    const id = document.getElementById("id").value;
+    const password = document.getElementById("password").value;
+}
+function closeModal() {
+    modal.style.display = "block";
+}
+/* Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+*/
+/* DIALOG Component
 const body = document.body;
 const openDialogBtn = document.getElementById("open-dialog");
 const closeDialogBtn = document.getElementById("close-dialog");
 const dialog = document.getElementById("my-dialog");
+const dialogP = document.querySelector("#dialogMsg");
 console.log("login.js load.");
 
 function logIn() {
     const id = document.getElementById("id").value;
     const password = document.getElementById("password").value;
-    if(id=="" ) {
-        dialog.showModal();
-        body.style.opacity = "0.2";
-    } 
-    console.log(password);
-    
+    if(id=="") {
+        openDialog("아이디를 입력해주세요.");
+    }else if(password=="") {
+        openDialog("비밀번호를 입력해주세요.");
+    }    
 }
-function signUp() {
-    window.location.replace("signup.html");
+
+function openDialog(msg) {
+    console.log(msg);    
+    dialogP.innerHtml = msg;
+    dialog.showModal();    
+    body.style.opacity = "0.2";
 }
 
 function closeDialog() {
     dialog.close();
     body.style.opacity = "1";
 }
-
-
-/*
-    if (id == "" && password == "") {        
-        openDialogBtn.addEventListener("click", () => {
-            dialog.showModal();
-            body.style.opacity = "0.2";
-        });
-    } */   
+*/
